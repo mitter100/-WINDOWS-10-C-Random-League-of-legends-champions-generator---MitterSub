e138cec728e523e5dce483314d80eea75d237a8e
mitter100 <72518817+mitter100@users.noreply.github.com>
2022-02-13 14:29:57 +0100

    [WINDOWS 10 / C#] Random League of legends champions generator - MitterSub
    [WINDOWS 10 / C#] Random League of legends champions generator - MitterSub
    
    VirusTotal:
    
    • Random League of legends champions generator - MitterSub.msi : https://www.virustotal.com/gui/file/dcf3b772ca832aa8ed53c7006f74d4fa037342964cc1a72622d14dc3935709be?nocache=1
    
    • Form1.cs : 
    https://www.virustotal.com/gui/file/fdd786b819be42b7fbddb74f0a4a1e49d918674a82f86322ac55c69d5f1cd5bd/detection
    
    
    League of legends client version: V12.3
    
    Champions (158):
    - Aatrox
    - Ahri
    - Akali
    - Akshan
    - Alistar
    - Amumu
    - Anivia
    - Annie
    - Aphelios
    - Ashe
    - Aurelion Sol
    - Azir
    - Bard
    - Blitzcrank
    - Brand
    - Braum
    - Caitlyn
    - Camille
    - Cassiopeia
    - Cho'Gath
    - Corki
    - Darius
    - Diana
    - Dr. Mundo
    - Draven
    - Ekko
    - Elise
    - Evelynn
    - Ezreal
    - Fiddlesticks
    - Fiora
    - Fizz
    - Galio
    - Gangplank
    - Garen
    - Gnar
    - Gragas
    - Graves
    - Gwen
    - Hecarim
    - Heimerdinger
    - Illaoi
    - Irelia
    - Ivern
    - Janna
    - Jarvan IV
    - Jax
    - Jayce
    - Jhin
    - Jinx
    - Kai'sa
    - Kalista
    - Karma
    - Karthus
    - Kassadin
    - Katarina
    - Kayle
    - Kayn
    - Kennen
    - Kha'Zix
    - Kindred
    - Kled
    - Kog'Maw
    - LeBlanc
    - Lee Sin
    - Leona
    - Lillia
    - Lissandra
    - Lucian
    - Lulu
    - Lux
    - Malphite
    - Malzahar
    - Maokai
    - Master Yi
    - Miss Fortune
    - Mordekaiser
    - Morgana
    - Nami
    - Nasus
    - Nautilus
    - Neeko
    - Nidalee
    - Nocturne
    - Nunu
    - Olaf
    - Orianna
    - Ornn
    - Pantheon
    - Poppy
    - Pyke
    - Qiyana
    - Quinn
    - Rakan
    - Rammus
    - Rek'Sai
    - Rell
    - Renekton
    - Rengar
    - Riven
    - Rumble
    - Ryze
    - Samira
    - Sejuani
    - Senna
    - Seraphine
    - Sett
    - Shaco
    - Shen
    - Shyvana
    - Singed
    - Sion
    - Sivir
    - Skarner
    - Sona
    - Soraka
    - Swain
    - Sylas
    - Syndra
    - Tahm Kench
    - Taliyah
    - Talon
    - Taric
    - Teemo
    - Thresh
    - Tristana
    - Trundle
    - Tryndamere
    - Twisted Fate
    - Twitch
    - Udyr
    - Urgot
    - Varus
    - Vayne
    - Veigar
    - Vel'Koz
    - Vex
    - Vi
    - Viego
    - Viktor
    - Vladimir
    - Volibear
    - Warwick
    - Wukong
    - Xayah
    - Xerath
    - Xin Zhao
    - Yasuo
    - Yone
    - Yorick
    - Yuumi
    - Zac
    - Zed
    - Zeri
    - Ziggs
    - Zilean
    - Zoe
    - Zyra
    
    Lanes:
    • Top
    • Jungle
    • Middle
    • Bottom
    • Support

---
+++ Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lol_random_champs
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Image.FromFile(Path.Combine(Environment.CurrentDirectory, @"img", "Random.png"));
+            pictureBox2.Image = Image.FromFile(Path.Combine(Environment.CurrentDirectory, @"img", "Random.png"));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String[] champions = {
+            "Aatrox",
+            "Ahri",
+            "Akali",
+            "Akshan",
+            "Alistar",
+            "Amumu",
+            "Anivia",
+            "Annie",
+            "Aphelios",
+            "Ashe",
+            "Aurelion Sol",
+            "Azir",
+            "Bard",
+            "Blitzcrank",
+            "Brand",
+            "Braum",
+            "Caitlyn",
+            "Camille",
+            "Cassiopeia",
+            "Cho'Gath",
+            "Corki",
+            "Darius",
+            "Diana",
+            "Dr. Mundo",
+            "Draven",
+            "Ekko",
+            "Elise",
+            "Evelynn",
+            "Ezreal",
+            "Fiddlesticks",
+            "Fiora",
+            "Fizz",
+            "Galio",
+            "Gangplank",
+            "Garen",
+            "Gnar",
+            "Gragas",
+            "Graves",
+            "Gwen",
+            "Hecarim",
+            "Heimerdinger",
+            "Illaoi",
+            "Irelia",
+            "Ivern",
+            "Janna",
+            "Jarvan IV",
+            "Jax",
+            "Jayce",
+            "Jhin",
+            "Jinx",
+            "Kai'sa",
+            "Kalista",
+            "Karma",
+            "Karthus",
+            "Kassadin",
+            "Katarina",
+            "Kayle",
+            "Kayn",
+            "Kennen",
+            "Kha'Zix",
+            "Kindred",
+            "Kled",
+            "Kog'Maw",
+            "LeBlanc",
+            "Lee Sin",
+            "Leona",
+            "Lillia",
+            "Lissandra",
+            "Lucian",
+            "Lulu",
+            "Lux",
+            "Malphite",
+            "Malzahar",
+            "Maokai",
+            "Master Yi",
+            "Miss Fortune",
+            "Mordekaiser",
+            "Morgana",
+            "Nami",
+            "Nasus",
+            "Nautilus",
+            "Neeko",
+            "Nidalee",
+            "Nocturne",
+            "Nunu",
+            "Olaf",
+            "Orianna",
+            "Ornn",
+            "Pantheon",
+            "Poppy",
+            "Pyke",
+            "Qiyana",
+            "Quinn",
+            "Rakan",
+            "Rammus",
+            "Rek'Sai",
+            "Rell",
+            "Renekton",
+            "Rengar",
+            "Riven",
+            "Rumble",
+            "Ryze",
+            "Samira",
+            "Sejuani",
+            "Senna",
+            "Seraphine",
+            "Sett",
+            "Shaco",
+            "Shen",
+            "Shyvana",
+            "Singed",
+            "Sion",
+            "Sivir",
+            "Skarner",
+            "Sona",
+            "Soraka",
+            "Swain",
+            "Sylas",
+            "Syndra",
+            "Tahm Kench",
+            "Taliyah",
+            "Talon",
+            "Taric",
+            "Teemo",
+            "Thresh",
+            "Tristana",
+            "Trundle",
+            "Tryndamere",
+            "Twisted Fate",
+            "Twitch",
+            "Udyr",
+            "Urgot",
+            "Varus",
+            "Vayne",
+            "Veigar",
+            "Vel'Koz",
+            "Vex",
+            "Vi",
+            "Viego",
+            "Viktor",
+            "Vladimir",
+            "Volibear",
+            "Warwick",
+            "Wukong",
+            "Xayah",
+            "Xerath",
+            "Xin Zhao",
+            "Yasuo",
+            "Yone",
+            "Yorick",
+            "Yuumi",
+            "Zac",
+            "Zed",
+            "Zeri",
+            "Ziggs",
+            "Zilean",
+            "Zoe",
+            "Zyra"};
+
+            String[] lanes = { "Top", "Jungle", "Middle", "Bottom", "Support" };
+
+            Random random_champ = new Random();
+            Random random_lane = new Random();
+
+            string champion_name = champions[random_champ.Next(champions.Length)];
+            string lane_name = lanes[random_lane.Next(lanes.Length)];
+
+            for (int i = 0; i < champions.Length; i++)
+            {
+                if (champion_name == champions[i])
+                {
+                    pictureBox1.Image = Image.FromFile(Path.Combine(Environment.CurrentDirectory, @"img\champions", champions[i] + ".png"));
+                }
+            }
+
+
+            for (int i = 0; i < lanes.Length; i++)
+            {
+                if (lane_name == lanes[i])
+                {
+                    pictureBox2.Image = Image.FromFile(Path.Combine(Environment.CurrentDirectory, @"img\lanes\", lanes[i] + ".png"));
+                }
+            }
+
+
+            textBox1.Text = champion_name;
+            textBox2.Text = lane_name;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
